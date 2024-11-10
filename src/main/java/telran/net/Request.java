128a8c82e91198bced4ebd9236433d5b0e4bc7e0
AlexeiRoginskii <roginskiial@gmail.com>
2024-11-10 21:33:09 +0200

    initilize project

---
+++ src/main/java/telran/net/Request.java
@@ -0,0 +1,13 @@
+package telran.net;
+
+import org.json.*;
+import static telran.net.TcpConfigurationProperties.*;
+public record Request(String requestType, String requestData) {
+    @Override
+    public String toString() {
+        JSONObject json = new JSONObject();
+        json.put(REQUEST_TYPE_FIELD, requestType);
+        json.put(REQUEST_DATA_FIELD, requestData);
+        return json.toString();
+    }
+}
